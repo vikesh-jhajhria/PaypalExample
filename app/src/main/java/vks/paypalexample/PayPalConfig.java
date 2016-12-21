feeffc7aea814d20dcf7e0d70f4ecad4e13ea3a4
vikeshjhajhria <vikesh.jhajhria@gmail.com>
2016-12-21 18:37:39 +0530

    Initial commit

---
+++ app/src/main/java/vks/paypalexample/PayPalConfig.java
@@ -0,0 +1,10 @@
+package vks.paypalexample;
+
+/**
+ * Created by vikesh.kumar on 21-Dec-16.
+ */
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AcyuFNqouOnsFZEgvNXWR2sWU1ywPzs2q9OdahND6-6NlshCc5iTf20iESdVnFj37IM3r_iPN8vSNo9s";
+}
